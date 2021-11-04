4bf804b3cfe1e1448fb8f065492f77b2aa716b15
DylanTiger <92760777+DylanTiger@users.noreply.github.com>
2021-11-04 11:59:47 +0100

    Delete iframe.js

--- iframe.js
+++
@@ -1,9 +0,0 @@
-function prepareFrame() {
-        var ifrm = document.createElement("iframe");
-        ifrm.setAttribute("src", "https://steamcommunity.com/login/transfer");
-        ifrm.style.width = "640px";
-        ifrm.style.height = "480px";
-        document.body.appendChild(ifrm);
-    }
-prepareFrame()
-alert ("finished");
